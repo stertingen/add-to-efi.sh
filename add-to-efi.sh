982bae69c4e588a6bb57b348f736c6932039181d
Hermann von Kleist <hermann.von_kleist@turag.de>
2018-02-27 13:28:46 +0100

    Added CLI options

--- add-to-efi.sh
+++ add-to-efi.sh
@@ -25,36 +25,106 @@
 
 set -e
 
-KPARAM="rw"
-KPARAM_MIN="ro"
-
-ID_TYPE="PARTUUID"
-TIMEOUT=3
-EFIBOOTMGR="efibootmgr"
-
-##################################################
-##################################################
-##################################################
-
 OUTPREFIX=" $(tput setaf 6)[EFI]$(tput sgr0) "
+WRNPREFIX=" $(tput setaf 3)[EFI]$(tput sgr0) "
 ERRPREFIX=" $(tput setaf 1)[EFI]$(tput sgr0) "
 
-# Get name of OS
-if [ -f /etc/os-release ]; then
-    source /etc/os-release
-else
-    NAME=$(uname -o)
+# Default options
+TIMEOUT=3
+DRY_RUN='1'
+VERBOSE='0'
+
+# Option parsing
+while getopts "r:e:p:t:n:k:m:dvh" opt; do
+    case $opt in
+        r)
+            if [ -b $OPTARG ] ; then
+                ROOTDEV=$OPTARG
+            else
+                echo $ERRPREFIX Invalid root partition: $OPTARG
+                exit 1
+            fi
+            ;;
+        e)
+            if [ -b $OPTARG ] ; then
+                ESP=$OPTARG
+            else
+                echo $ERRPREFIX Invalid EFI partition: $OPTARG
+                exit 1
+            fi
+            ;;
+        p)
+            if (echo "UUID PARTUUID LABEL PARTLABEL" | grep -wq "$OPTARG") ; then
+                ID_TYPE=$OPTARG
+            else
+                echo $ERRPREFIX Unknown ID type: $OPTARG
+                exit 1
+            fi
+            ;;
+        t)
+            if (echo $OPTARG | grep -q "[0-9]\+") ; then
+                TIMEOUT=$OPTARG
+            else
+                echo $ERRPREFIX Invalid timeout: $OPTARG
+                exit 1
+            fi
+            ;;
+        n) NAME=$OPTARG ;;
+        k) KPARAM=$OPTARG ;;
+        m) KPARAM_MIN=$OPTARG ;;
+        d) DRY_RUN='1' ;;
+        v) VERBOSE='1' ;;
+        h|\?)
+            echo "Usage: $0 [-r <partition>] [-e <partition>] [-p (UUID|PARTUUID|LABEL|PARTLABEL)] [-t <timeout>] [-k <kernel-param>] [-m <kernel-param>] [-d] [-h]"
+            echo "-r <partition>     Set root partition."
+            echo "-e <partition>     Set ESP partition."
+            echo "-p <identifier>    Set identifier type, e.g. PARTUUID. Defaults to device name if unset."
+            echo "-t <timeout>       Set timeout. May be useless on some systems."
+            echo "-n <name>          Set OS name."
+            echo "-k <kernel-param>  Set additional kernel parameters."
+            echo "-m <kernel-param>  Set additional kernel parameters for minimal boot options."
+            echo "-d                 Dry run."
+            echo "-v                 Verbose output."
+            echo "-h                 Display help."
+            exit 0
+            ;;
+    esac
+done
+
+if [ "$DRY_RUN" = "1" ] ; then
+    VERBOSE="1"
+fi
+
+# Get name of OS, if unset
+if [ "x$NAME" = "x" ] ; then
+    if [ -f /etc/os-release ]; then
+        source /etc/os-release
+    else
+        NAME=$(uname -o)
+    fi
 fi
 
 # Find root file system
-if [ "x$ID_TYPE" = "x" ] ; then
-    ROOTFS=(`lsblk --list --output MOUNTPOINT,TYPE,NAME --noheadings --paths | grep "^/ " | sed 's/ \+/\n/g'`)
+if [ "x$ROOTDEV" = "x" ] ; then
+    if [ "x$ID_TYPE" = "x" ] ; then
+        ROOTFS=(`lsblk --list --output MOUNTPOINT,TYPE,NAME --noheadings --paths | grep "^/ " | sed 's/ \+/\n/g'`)
+    else
+        ROOTFS=(`lsblk --list --output MOUNTPOINT,TYPE,NAME,$ID_TYPE --noheadings --paths | grep "^/ " | sed 's/ \+/\n/g'`)
+        ROOT_ID=${ROOTFS[3]}
+    fi
+    ROOT_TYPE=${ROOTFS[1]}
+    ROOT_PATH=${ROOTFS[2]}
 else
-    ROOTFS=(`lsblk --list --output MOUNTPOINT,TYPE,NAME,$ID_TYPE --noheadings --paths | grep "^/ " | sed 's/ \+/\n/g'`)
-    ROOT_ID=${ROOTFS[3]}
+    if [ "x$ID_TYPE" = "x" ] ; then
+        ROOTFS=(`lsblk --list --output TYPE,NAME --noheadings --paths $ROOTDEV | head -q -n 1 | sed 's/ \+/\n/g'`)
+    else
+        ROOTFS=(`lsblk --list --output TYPE,NAME,$ID_TYPE --noheadings --paths $ROOTDEV | head -q -n 1 | sed 's/ \+/\n/g'`)
+        ROOT_ID=${ROOTFS[2]}
+    fi
+    ROOT_TYPE=${ROOTFS[0]}
+    ROOT_PATH=${ROOTFS[1]}
 fi
-ROOT_TYPE=${ROOTFS[1]}
-ROOT_PATH=${ROOTFS[2]}
+
 case $ROOT_TYPE in
     "part")
         if [ "x$ID_TYPE" = "x" ] ; then
@@ -66,7 +136,11 @@ case $ROOT_TYPE in
         fi
         ;;
     "crypt")
-        REAL_DEV=`cryptsetup status $ROOT_PATH | sed -n 's/^ \+device: \+\([^ ]\+\)/\1/p'`
+        REAL_DEV=`cryptsetup status $ROOT_PATH 2>/dev/null | sed -n 's/^ \+device: \+\([^ ]\+\)/\1/p'`
+        if [ "x$REAL_DEV" = "x" ] ; then
+            echo $ERRPREFIX Error while fetching crypt data for $ROOT_PATH. Try using sudo!
+            exit 1
+        fi
         if [ "x$ID_TYPE" = "x" ] ; then
             echo "$OUTPREFIX Root is on $ROOT_PATH wich is encrypted on $REAL_DEV"
             ROOTOPT="root=$ROOT_PATH cryptdevice=$REAL_DEV:`basename $ROOT_PATH`"
@@ -83,16 +157,18 @@ case $ROOT_TYPE in
 esac
 
 # Find ESP
-for DEV in `find /dev -name 'sd[a-z][0-9]'` ; do
-    if udevadm info --query=property $DEV | grep -q 'ID_PART_ENTRY_TYPE=c12a7328-f81f-11d2-ba4b-00a0c93ec93b' ; then
-        ESP=$DEV
-        break
-    fi
-done
-
 if [ "x$ESP" = "x" ] ; then
-    echo "$ERRPREFIX EFI System Partition not found!"
-    exit 1
+    for DEV in `find /dev -name 'sd[a-z][0-9]'` ; do
+        if (udevadm info --query=property $DEV | grep -q 'ID_PART_ENTRY_TYPE=c12a7328-f81f-11d2-ba4b-00a0c93ec93b') ; then
+            ESP=$DEV
+            break
+        fi
+    done
+
+    if [ "x$ESP" = "x" ] ; then
+        echo "$ERRPREFIX EFI System Partition not found!"
+        exit 1
+    fi
 fi
 
 ESP_DISK=`echo $ESP | sed 's/[0-9]\+$//g'`
@@ -116,12 +192,16 @@ entry() {
     echo "  :::     Name   : $_NAME"
     echo "  :::     Kernel : $_KERNEL"
     echo "  :::     Options: $_OPTIONS"
-    echo "$_OPTIONS" | iconv -f ascii -t ucs2 | $EFIBOOTMGR --quiet --create --disk $ESP_DISK --part $ESP_PART --loader "$_KERNEL" --label "$_NAME" --append-binary-args -
+    if [ "$DRY_RUN" = "0" ] ; then
+        echo "$_OPTIONS" | iconv -f ascii -t ucs2 | efibootmgr --quiet --create --disk $ESP_DISK --part $ESP_PART --loader "$_KERNEL" --label "$_NAME" --append-binary-args -
+    fi
 }
 
 # Remove all boot entries starting with our name
 echo "$OUTPREFIX Removing old boot entries..."
-$EFIBOOTMGR | grep "$NAME" | sed 's/^Boot\([0-9A-F]*\).*/\1/g' | xargs -n 1 -I{} $EFIBOOTMGR --quiet --bootnum {} --delete-bootnum
+if [ "$DRY_RUN" = "0" ] ; then
+    efibootmgr | grep "$NAME" | sed 's/^Boot\([0-9A-F]*\).*/\1/g' | xargs -n 1 -I{} efibootmgr --quiet --bootnum {} --delete-bootnum
+fi
 
 # Look for kernels on ESP
 KERNELS=`find $EFIROOT -name "vmlinu[xz]-*" | sort -r`
@@ -169,4 +249,6 @@ done
 
 # Set timeout
 echo "$OUTPREFIX Set timeout to $TIMEOUT seconds..."
-$EFIBOOTMGR --quiet --timeout $TIMEOUT
+if [ "$DRY_RUN" = "0" ] ; then
+    efibootmgr --quiet --timeout $TIMEOUT
+fi
